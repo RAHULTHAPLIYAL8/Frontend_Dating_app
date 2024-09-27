6a6deb21a8c4696096bab5bc82d53fb4a56103bc
RAHULTHAPLIYAL8 <rahulthapliyal47@gmail.com>
2024-09-27 11:00:17 +0530

    change on code

--- frontend/src/Components/Dashboard.jsx
+++ frontend/src/Components/Dashboard.jsx
@@ -1,24 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ApprovalPending from './ApprovalPending';
 import NavBar from './NavBar';
 import { useParams } from 'react-router-dom';
 
 export default function Dashboard() {
     const { status } = useParams();  
-    const [isApprovalClear, setIsApprovalClear] = useState(false);
-   
-    useEffect(() => {
-        if (status === "approve") {
-            setIsApprovalClear(true);
-        } else {
-            setIsApprovalClear(false); // Reset state if status is not "approve"
-        }
-    }, [status]); // Run effect when status changes
-    
+
     return (
         <div>
-            <NavBar />
-            {isApprovalClear ? <h1>The approval status is approve</h1> : <ApprovalPending />}
+            {status === "approve" ? (
+                <>
+                    <NavBar />
+                    <h1>The approval status is approve</h1>
+                </>
+            ) : (
+                <ApprovalPending />
+            )}
         </div>
     );
-} 
+}
